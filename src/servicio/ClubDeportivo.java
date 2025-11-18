e257391a4145456618a7e899a51d507fdacec91b
Alejandro Sandoval <2860045@alu.murciaeduca.es>
2025-11-18 18:59:25 +0100

    Metodo altaPista realizado correctamente

--- src/servicio/ClubDeportivo.java
+++ src/servicio/ClubDeportivo.java
@@ -41,10 +41,11 @@ public class ClubDeportivo {
 
         } else {
             //Comprobamos si el socio ya existe en la base de datos
-            String sqlComprobar = "SELECT COUNT(*) FROM socios WHERE id_socio = ? OR dni = ?";
+            String sqlComprobar = "SELECT COUNT(*) FROM socios WHERE id_socio = ? OR dni = ? OR email = ?";
             PreparedStatement pstComprobar = conexion.prepareStatement(sqlComprobar);
             pstComprobar.setString(1, socio.getIdSocio());
             pstComprobar.setString(2, socio.getDni());
+            pstComprobar.setString(3, socio.getEmail());
 
             ResultSet rs = pstComprobar.executeQuery();
             rs.next();
@@ -56,7 +57,7 @@ public class ClubDeportivo {
 
 
                 //Creamos la consulta para poder insertar un nuevo socio en la base de datos
-                String sql = "INSERT INTO socios(id_socio,dni,nombre,apellido,telefono,email) VALUES (?,?,?,?,?,?)";
+                String sql = "INSERT INTO socios(id_socio,dni,nombre,apellidos,telefono,email) VALUES (?,?,?,?,?,?)";
 
                 PreparedStatement pst = null;
 
@@ -101,10 +102,11 @@ public class ClubDeportivo {
 
         } else {
             //Comprobamos si el socio ya existe en la base de datos
-            String sqlComprobar = "SELECT COUNT(*) FROM socios WHERE id_socio = ? OR dni = ?";
+            String sqlComprobar = "SELECT COUNT(*) FROM socios WHERE id_socio = ? OR dni = ? OR email = ?";
             PreparedStatement pstComprobar = conexion.prepareStatement(sqlComprobar);
             pstComprobar.setString(1, socio.getIdSocio());
             pstComprobar.setString(2, socio.getDni());
+            pstComprobar.setString(3, socio.getEmail());
 
             ResultSet rs = pstComprobar.executeQuery();
             rs.next();
@@ -120,6 +122,46 @@ public class ClubDeportivo {
                 pst.executeUpdate();
                 return true;
             }
+        }
+    }
+    /**
+     * Inserta una nueva pista en la base de datos
+     *
+     * @param pista Pista a insertar
+     * @return true si se ha insertado correctamente, false en caso contrario
+     * @throws SQLException si hay un error al conectarse con la base de datos
+     * @throws IdObligatorioException si el objeto Pista o su ID es nulo o está vacío
+     * @author Alejandro
+     */
+    public boolean altaPista(Pista pista) throws SQLException,IdObligatorioException {
+        //Validamos que el objeto pista no sea null
+        if (pista==null){
+            throw new IdObligatorioException("Pista no puede ser null");
+        }else if(pista.getIdPista()==null || pista.getIdPista().isBlank()){
+            throw new IdObligatorioException("ID de la pista es obligatorio");
+        }else{
+            //Comprobamos si pista ya existe
+            String sqlComprobacion="SELECT COUNT(*) FROM pistas WHERE id_pista=?";
+            PreparedStatement pstComprobacion=conexion.prepareStatement(sqlComprobacion);
+            pstComprobacion.setString(1,pista.getIdPista());
+            ResultSet rs=pstComprobacion.executeQuery();
+            rs.next();
+            if(rs.getInt(1)>0){
+                return false;//Si existe devolvemos false
+            }else{
+                //Si no existe insertamos
+                String sql="INSERT INTO pistas(id_pista,deporte,descripcion,disponible) VALUES (?,?,?,?)";
+                PreparedStatement pst=conexion.prepareStatement(sql);
+                pst.setString(1,pista.getIdPista());
+                pst.setString(2,pista.getDeporte());
+                pst.setString(3,pista.getDescripcion());
+                pst.setBoolean(4,pista.isDisponible());
+                pst.executeUpdate();
+                return true;//Si no existe devolvemos true
+
+            }
+
+
         }
     }
 
